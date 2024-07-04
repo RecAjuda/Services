a8e195e3a820f1c702fd7dcf353894be9de81570
Davi Freitas <davimello14@gmail.com>
2024-07-04 08:26:17 -0300

    [Feat] update shelter

---
+++ RecAjuda.API/Shelters/UpdateShelterById/UpdateShelterByIdCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using BuildingBlocks.CQRS;
+using RecAjuda.API.Exception.Shelter;
+using RecAjuda.API.Shelters.CreateShelter;
+using RecAjuda.API.ValueObjects;
+
+namespace RecAjuda.API.Shelters.UpdateShelterById;
+
+public record UpdateShelterByIdCommand(
+    string Name,
+    ShelterType Type,
+    double Latitude,
+    double Longitude,
+    string Neighborhood,
+    string Street,
+    List<AgeRange> AgeRanges,
+    int Capacity,
+    string WorkingHourDescription,
+    List<ContactModel> Contacts,
+    Guid Id) : ICommand<UpdateShelterByIdResult>
+{
+    public void UpdateModel(Shelter shelterForUpdate)
+    {
+        IEnumerable<Contact> contactsFromModel = Contacts.Select(x => new Contact(x.PreFixNumber, x.Number));
+
+        shelterForUpdate.Update(Name, Type, new GeoCoordinate(Latitude, Longitude), Capacity, WorkingHourDescription,
+            AgeRanges, contactsFromModel);
+    }
+}
+
+public record UpdateShelterByIdResult(Guid Id);
+
+public class UpdateShelterByIdCommandHandler(IDocumentSession session)
+    : ICommandHandler<UpdateShelterByIdCommand, UpdateShelterByIdResult>
+{
+    public async Task<UpdateShelterByIdResult> Handle(UpdateShelterByIdCommand request,
+        CancellationToken cancellationToken)
+    {
+        Shelter? shelterForUpdate = await session.Query<Shelter>()
+            .FirstOrDefaultAsync(s => s.Id == request.Id, token: cancellationToken);
+
+        if (shelterForUpdate is null)
+            throw new ShelterNotFoundException();
+
+        request.UpdateModel(shelterForUpdate);
+        
+        session.Update(shelterForUpdate);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new UpdateShelterByIdResult(shelterForUpdate.Id);
+    }
+}
---
+++ RecAjuda.API/Shelters/UpdateShelterById/UpdateShelterByIdEndPoint.cs
@@ -0,0 +1,48 @@
+﻿using RecAjuda.API.Shelters.CreateShelter;
+
+namespace RecAjuda.API.Shelters.UpdateShelterById;
+
+public record UpdateShelterByIdRequest(
+    string Name,
+    ShelterType Type,
+    double Latitude,
+    double Longitude,
+    string Neighborhood,
+    string Street,
+    List<AgeRange> AgeRanges,
+    int Capacity,
+    string WorkingHourDescription,
+    List<ContactModel> Contacts)
+{
+    public UpdateShelterByIdCommand ConvertToCommandEntity(Guid id)
+    {
+        UpdateShelterByIdCommand command = new UpdateShelterByIdCommand(Name, Type, Latitude,
+            Longitude, Neighborhood, Street, AgeRanges, Capacity,
+            WorkingHourDescription, Contacts, id);
+
+        return command;
+    }
+}
+
+public record UpdateShelterByIdResponse(Guid Id);
+
+public class UpdateShelterByIdEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/shelter/{id}", async (Guid id, UpdateShelterByIdRequest request, ISender sender) =>
+            {
+                UpdateShelterByIdCommand command = request.ConvertToCommandEntity(id);
+
+
+                UpdateShelterByIdResult result = await sender.Send(command);
+
+                UpdateShelterByIdResponse response = new UpdateShelterByIdResponse(result.Id);
+
+                return Results.Ok(response);
+            }).WithName("Update shelter endpoint")
+            .WithDescription("Update shelter endpoint")
+            .Produces<UpdateShelterByIdResponse>()
+            .ProducesProblem(StatusCodes.Status500InternalServerError);
+    }
+}
