e806764ffd77c6d320ede5f4659b9b8a193ba32d
Davi Freitas <davimello14@gmail.com>
2024-07-04 08:22:38 -0300

    [Feat] delete shelteer

---
+++ RecAjuda.API/Shelters/DeleteShelter/DeleteShelterCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.Base;
+using BuildingBlocks.CQRS;
+using RecAjuda.API.Exception.Shelter;
+
+namespace RecAjuda.API.Shelters.DeleteShelter;
+
+public record DeleteShelterCommand(Guid Id) : ICommand<DeleteShelterResult>;
+
+public record DeleteShelterResult(bool Result);
+
+public class DeleteShelterCommandHandler(IDocumentSession session)
+    : ICommandHandler<DeleteShelterCommand, DeleteShelterResult>
+{
+    public async Task<DeleteShelterResult> Handle(DeleteShelterCommand request, CancellationToken cancellationToken)
+    {
+        Shelter? shelterForRemove = await session.Query<Shelter>()
+            .FirstOrDefaultAsync(x => x.Id == request.Id, token: cancellationToken);
+
+        if (shelterForRemove is null)
+            throw new ShelterNotFoundException();
+
+        shelterForRemove.Delete();
+
+        try
+        {
+            session.Update(shelterForRemove);
+            await session.SaveChangesAsync(cancellationToken);
+        }
+        catch (System.Exception genericException)
+        {
+            return new DeleteShelterResult(false);
+        }
+
+        return new DeleteShelterResult(true);
+    }
+}
---
+++ RecAjuda.API/Shelters/DeleteShelter/DeleteShelterEndPoint.cs
@@ -0,0 +1,20 @@
+﻿namespace RecAjuda.API.Shelters.DeleteShelter;
+
+public record DeleteShelterResponse(bool IsDeleted);
+
+public class DeleteShelterEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/shelters/{id}", async (Guid id, ISender sender) =>
+            {
+                DeleteShelterResult result = await sender.Send(new DeleteShelterCommand(id));
+
+                DeleteShelterResponse response = new DeleteShelterResponse(result.Result);
+                return Results.Ok(response);
+            }).WithName("Delete shelter")
+            .Produces<DeleteShelterResponse>()
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithDescription("Delete shelter endpoint");
+    }
+}
