3aa72a2355e6813e8227c97124f30513ac8c1df3
Davi Freitas <davimello14@gmail.com>
2024-07-04 08:22:49 -0300

    [Feat] get shelter by id

---
+++ RecAjuda.API/Shelters/GetShelterById/GetShelterByIdEndPoint.cs
@@ -0,0 +1,20 @@
+﻿namespace RecAjuda.API.Shelters.GetShelterById;
+
+
+public record GetShelterByIdResponse(Shelter Shelter);
+
+public class GetShelterByIdEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/shelter/{id}", async (Guid id, ISender sender) =>
+            {
+                GetShelterByIdResult result = await sender.Send(new GetShelterByIdQuery(id));
+                GetShelterByIdResponse response = new(result.Shelter);
+                return Results.Ok(response);
+            }).WithName("Get shelter by id ")
+            .Produces<GetShelterByIdResponse>()
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithDescription("Get shelter by id");
+    }
+}
---
+++ RecAjuda.API/Shelters/GetShelterById/GetShelterByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks.CQRS;
+using RecAjuda.API.Exception.Shelter;
+
+namespace RecAjuda.API.Shelters.GetShelterById;
+
+public record GetShelterByIdQuery(Guid Id) : IQuery<GetShelterByIdResult>;
+
+public record GetShelterByIdResult(Shelter Shelter);
+
+public class GetShelterByIdQueryHandler(IDocumentSession session)
+    : IQueryHandler<GetShelterByIdQuery, GetShelterByIdResult>
+{
+    public async Task<GetShelterByIdResult> Handle(GetShelterByIdQuery request, CancellationToken cancellationToken)
+    {
+        Shelter? shelterByIdForReturn = await session.Query<Shelter>()
+            .FirstOrDefaultAsync(shelter => shelter.Id == request.Id, token: cancellationToken);
+
+        if (shelterByIdForReturn is null)
+            throw new ShelterNotFoundException();
+
+        return new GetShelterByIdResult(shelterByIdForReturn);
+    }
+}
