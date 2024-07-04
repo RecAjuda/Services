dbe2865a463665b767d336bacce53b83352ddb7b
Davi Freitas <davimello14@gmail.com>
2024-07-04 08:22:59 -0300

    [Feat] get shelters

---
+++ RecAjuda.API/Shelters/GetShelters/GetSheltersEndPoint.cs
@@ -0,0 +1,20 @@
+﻿namespace RecAjuda.API.Shelters.GetShelters;
+
+
+public record GetShelterResponse(IEnumerable<Shelter> Shelters);
+public class GetSheltersEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/shelters", async (ISender sender) =>
+            {
+                GetSheltersResult getSheltersResult = await sender.Send(new GetSheltersQuery());
+                GetShelterResponse response = new(getSheltersResult.SheltersList);
+
+                return Results.Ok(response);
+            }).WithName("get shelters")
+            .Produces<GetShelterResponse>()
+            .WithDescription("Get all shelters in database")
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
---
+++ RecAjuda.API/Shelters/GetShelters/GetSheltersQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.Base;
+using BuildingBlocks.CQRS;
+using RecAjuda.API.Models;
+
+namespace RecAjuda.API.Shelters.GetShelters;
+
+public record GetSheltersQuery() : IQuery<GetSheltersResult>;
+public record GetSheltersResult(IEnumerable<Shelter> SheltersList);
+
+public class GetSheltersQueryHandler(IDocumentSession session) : IQueryHandler<GetSheltersQuery, GetSheltersResult>
+{
+    public async Task<GetSheltersResult> Handle(GetSheltersQuery request, CancellationToken cancellationToken)
+    {
+        IEnumerable<Shelter> sheltersReturned = await session.Query<Shelter>().ToListAsync(token: cancellationToken);
+
+        sheltersReturned = sheltersReturned.Where(shelter => shelter.StatusDefault == StatusDefault.Active);
+        return new GetSheltersResult(sheltersReturned);
+    }
+}
